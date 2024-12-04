50563174fa53aab453c478ec89e91c24fb2ca8b1
chikien111 <38467294+chikien111@users.noreply.github.com>
2024-12-04 09:25:19 +0700

    update page

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/* HELPER: Checks Whether an Element Exists
+----------------------------------------------------------------------------------------------------*/
+(function( $ ) {
+
+  $.fn.extend({
+    exists: function() {
+      if ( this.length > 0 ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  });
+
+})( jQuery );
+
+
+
+jQuery(document).on("ready",function () {
+	
+});
+
+
+
+// $("#md-popup-1").fancybox().trigger('click');
+
+// $("#md-popup-2").fancybox().trigger('click');
+
+$("#md-popup-3").fancybox().trigger('click');
+
+// $("#md-popup-4").fancybox().trigger('click');
