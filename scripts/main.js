c05b5627c589280bb95fcd21bf254eb205906906
chikien111 <38467294+chikien111@users.noreply.github.com>
2024-12-05 09:26:29 +0700

    update page

--- scripts/main.js
+++ scripts/main.js
@@ -24,10 +24,21 @@ jQuery(document).on("ready",function () {
 
 
 
-$("#md-popup-1").fancybox().trigger('click');
+  // $("#md-popup-1").fancybox().trigger('click');
 
-$("#md-popup-2").fancybox().trigger('click');
+// $("#md-popup-2").fancybox().trigger('click');
 
-$("#md-popup-3").fancybox().trigger('click');
+// $("#md-popup-3").fancybox().trigger('click');
 
 // $("#md-popup-4").fancybox().trigger('click');
+
+
+let firstScroll = true;
+$(window).scroll(function() {
+    if (firstScroll) {
+        firstScroll = false;
+        $('html,body').animate({
+            scrollTop: $(".md-img-3").offset().top
+        }, 1000);
+    }
+});
